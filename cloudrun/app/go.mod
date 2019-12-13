a4c5680b06ea9aedc24c106b29b3d44ca4a84b1f
JUN SAKATA <jun.bj141400@gmail.com>
2019-12-14 03:08:27 +0900

    init

---
+++ cloudrun/app/go.mod
@@ -0,0 +1,8 @@
+module github.com/sakajunquality/devfest-tokyo-19-demo/cloudrun/app
+
+go 1.13
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/render v1.0.1
+)
---
+++ cloudrun/app/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+type Message struct {
+	Status int    `json:"status"`
+	Text   string `json:"text"`
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		m := &Message{
+			Status: http.StatusOK,
+			Text:   os.Getenv("MESSAGE"),
+		}
+		render.JSON(w, r, m)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
